957bbefbd8e42967316409bba3cf32d83a772362
Zoltan Altfatter <altfatterz@gmail.com>
2013-03-23 12:56:22 +0100

    add mysql version of schema file

---
+++ src/test/resources/schema-mysql.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS addresses;
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE addresses (
+  id            INT AUTO_INCREMENT,
+  street_name   VARCHAR(100) NOT NULL,
+  street_number INT          NOT NULL,
+  postcode      VARCHAR(10)  NOT NULL,
+  city          VARCHAR(50)  NOT NULL,
+  version INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE restaurants (
+  id         INT AUTO_INCREMENT,
+  name       VARCHAR(100) NOT NULL,
+  website    VARCHAR(100) NOT NULL,
+  address_id INT          NOT NULL,
+  version INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_restaurants_id FOREIGN KEY (address_id)
+  REFERENCES addresses (id)
+);
+CREATE UNIQUE INDEX uq_restaurants_name ON restaurants (name);
+
+CREATE TABLE reviews (
+  id            INT AUTO_INCREMENT,
+  restaurant_id INT,
+  user          VARCHAR(20)  NOT NULL,
+  description   VARCHAR(200) NOT NULL,
+  rate          TINYINT      NOT NULL,
+  created          DATE         NOT NULL,
+  version INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_reviews_restaurant_id FOREIGN KEY (restaurant_id)
+  REFERENCES restaurants (id)
+);
+
