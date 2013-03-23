646f61e9a4ce9245432fd4b3de4c561f149c9f57
Zoltan Altfatter <altfatterz@gmail.com>
2013-03-23 22:13:34 +0100

    fix for Exception translation to Spring's exception hierarchy

--- src/test/java/progfun/spring/data/repository/jpa/JpaRestaurantRepositoryTest.java
+++ src/test/java/progfun/spring/data/repository/jpa/JpaRestaurantRepositoryTest.java
@@ -3,6 +3,7 @@ package progfun.spring.data.repository.jpa;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import progfun.spring.data.domain.Address;
@@ -28,58 +29,58 @@ public class JpaRestaurantRepositoryTest extends AbstractTest {
     private RestaurantRepository repository;
 
     @Test
-    public void testFindById() {
+    public void findById() {
         Restaurant restaurant = repository.findOne(102L);
         assertThat(restaurant.getName(), is(equalTo("Cafe Olivier")));
         assertThat(restaurant.getReviews().size(), is(equalTo(2)));
     }
 
     @Test
-    public void testFindAll() {
+    public void findAll() {
         List<Restaurant> restaurants = repository.findAll();
         assertThat(restaurants.size(), is(equalTo(3)));
     }
 
     @Test
-    public void testFindResturantByWebsite() {
+    public void findResturantByWebsite() {
         Restaurant restaurant = repository.findByWebsite(new Website("http://www.leconnaisseur.nl"));
         assertThat(restaurant.getName(), is(equalTo("Le Connaisseur")));
     }
 
     @Test
-    public void testExists() {
+    public void exists() {
         assertThat(repository.exists(102L), is(equalTo(true)));
         assertThat(repository.exists(202L), is(equalTo(false)));
     }
 
     @Test
-    public void testCount() throws Exception {
+    public void count() throws Exception {
         assertThat(repository.count(), is(equalTo(3L)));
     }
 
     @Test
-    public void testDeleteById() throws Exception {
+    public void deleteById() throws Exception {
         assertThat(repository.count(), is(equalTo(3L)));
         repository.delete(102L);
         assertThat(repository.count(), is(equalTo(2L)));
     }
 
     @Test
-    public void testDelete() throws Exception {
+    public void delete() throws Exception {
         assertThat(repository.count(), is(equalTo(3L)));
         repository.delete(repository.findOne(102L));
         assertThat(repository.count(), is(equalTo(2L)));
     }
 
     @Test
-    public void testDeleteAll() throws Exception {
+    public void deleteAll() throws Exception {
         assertThat(repository.count(), is(equalTo(3L)));
         repository.deleteAll();
         assertThat(repository.count(), is(equalTo(0L)));
     }
 
     @Test
-    public void testSaveRestaurant() {
+    public void save() {
         Restaurant restaurant = new Restaurant();
         restaurant.setName("Ledig Erf");
         restaurant.setWebsite(new Website("http://www.ledigerf.nl"));
@@ -89,6 +90,11 @@ public class JpaRestaurantRepositoryTest extends AbstractTest {
         repository.save(restaurant);
 
         assertThat(restaurant.getId(), is(not(nullValue())));
+    }
+
+    @Test(expected = DataIntegrityViolationException.class)
+    public void saveIncompleteData() {
+        repository.save(new Restaurant());
     }
 
 }
